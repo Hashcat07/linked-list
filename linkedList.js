b41289374d13edcb31dc0f3557ed4faab01d338a
Ray <166890725+Hashcat07@users.noreply.github.com>
2025-09-23 20:39:21 +0530

    Completed Linked List

--- linkedList.js
+++ linkedList.js
@@ -1,53 +1,148 @@
 class LinkedList{
-
+constructor(){
+    this.headNode=null
+    this.tailNode=null
+}
     append(value){
-        
+        let newNode=new node(value)
+        if(this.headNode===null)
+        {
+            this.headNode=newNode
+            this.tailNode=newNode
+        }
+        else
+        {
+        let tail=this.tail()
+        tail.nextNode=newNode
+        this.tailNode=newNode
+        }
     }
     prepend(value){
-
+        let newNode=new node(value)
+        if(this.headNode===null){
+            this.headNode=newNode
+        }
+        else{
+        let head=this.head()
+        newNode.nextNode=head
+        this.headNode=newNode
+        }
+        
     }
     size()
     {
-
+        let length=0
+        let tmp=this.head()
+        while(tmp!=null)
+        {
+            length++
+            tmp=tmp.nextNode
+        }
+        return length
     }
     head()
     {
-
+        return this.headNode
     }
     tail()
     {
-
+        return this.tailNode
     }
     at(index){
-
+        let tmp=this.head()
+        let i=0
+        while(i!=index ){
+            if(tmp.nextNode===null) return null
+            tmp=tmp.nextNode
+            i++
+        }
+        return tmp
     }
     pop()
     {
-
+        let tmp=this.head()
+        if(tmp.nextNode===null) {
+            this.headNode=null
+            this.tailNode=null
+            return
+        }
+        if(this.headNode===null&&this.tailNode===null) return
+        while(tmp.nextNode.nextNode!==null)
+        {
+            tmp=tmp.nextNode
+        }
+        tmp.nextNode=null
+        this.tailNode=tmp
     }
     contains(value)
     {
-
+        let tmp= this.head()
+        if(tmp.value===value) return true
+        else
+        while(tmp.nextNode!==null){
+            if(tmp.value===value)
+            {
+                return true
+            }
+            tmp=tmp.nextNode
+        }
+        if(tmp.value==null) return false
+        return false
     }
     find(value)
     {
-        
+      let tmp=this.head()
+      let i=0
+      if(tmp.value===value) return i
+      else{
+        while(tmp.nextNode!==null)
+        {
+            if(tmp.value===value)
+                return i
+            tmp=tmp.nextNode
+            ++i
+        }
+        if(tmp.value===value) return i
+      }  
+      return "Not Found"
     }
     toString()
     {
-
+        let tmp=this.head()
+        let list=""
+        if(this.headNode===null&&this.tailNode===null)return "Empty List"
+        while(tmp.nextNode!=null)
+        {
+            list+=`${tmp.value}->`
+            tmp=tmp.nextNode
+        }
+        return list+`${tmp.value}->null`
     }
     insertAt(value,index){
-
+        let tmp=this.head()
+        let atIndex=this.at(index)
+        while(tmp.nextNode!=atIndex)
+        {
+            tmp=tmp.nextNode
+        }
+        let newNode=new node(value,atIndex)
+        tmp.nextNode=newNode
     }
     removeAt(index){
-        
+        let tmp=this.head()
+        let atIndex=this.at(index)
+        while(tmp.nextNode!=atIndex)
+            tmp=tmp.nextNode
+        tmp.nextNode=atIndex.nextNode
     }
 }
 
 
 class node{
     constructor (value=null,nextNode=null){
-
+        this.value=value
+        this.nextNode=nextNode
     }
 }
+
+export {LinkedList}
